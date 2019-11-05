1d5e1ae7e91ba762ccdcf18d92abc995f321ceb7
carloshenche <carloshenche@metricool.com>
2019-11-05 14:52:52 +0100

    Added customer mapper

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Customer.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbrewery.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID uuid;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String customerName;
+}
